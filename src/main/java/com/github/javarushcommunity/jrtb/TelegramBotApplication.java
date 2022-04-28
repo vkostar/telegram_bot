53f58180451a25ab8aa6cf31f5a4c97bc6a35b77
”konstantin <vihroff@gmail.com>
2022-04-28 23:49:08 +0300

    add properties

--- src/main/java/com/github/javarushcommunity/jrtb/TelegramBotApplication.java
+++ src/main/java/com/github/javarushcommunity/jrtb/TelegramBotApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavarushTelegramBotApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavarushTelegramBotApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        SpringApplication.run(JavarushTelegramBotApplication.class, args);
+    }
 
 }
