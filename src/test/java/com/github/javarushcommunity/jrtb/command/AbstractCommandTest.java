525471b7ca90a5947857cf9da7f24016ae3ff5e5
vkostar <82523029+vkostar@users.noreply.github.com>
2022-04-30 17:02:43 +0300

    Jrtb 3 (#14)
    * JRTB-2: updated project version and added to RELEASE_NOTES
    
    * JRTB-3: updated2
    
    * JRTB-3: updated4
    
    * JRTB-3: updated5

---
+++ src/test/java/com/github/javarushcommunity/jrtb/command/AbstractCommandTest.java
@@ -0,0 +1,50 @@
+package com.github.javarushcommunity.jrtb.command;
+
+
+import com.github.javarushcommunity.jrtb.bot.KostarTelegramBot;
+import com.github.javarushcommunity.jrtb.service.SendBotMessageService;
+import com.github.javarushcommunity.jrtb.service.SendBotMessageServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+/**
+ * Abstract class for testing {@link Command}s.
+ */
+abstract class AbstractCommandTest {
+
+    protected KostarTelegramBot bot = Mockito.mock(KostarTelegramBot.class);
+    protected SendBotMessageService sendBotMessageService = new SendBotMessageServiceImpl(bot);
+
+    abstract String getCommandName();
+
+    abstract String getCommandMessage();
+
+    abstract Command getCommand();
+
+    @Test
+    public void shouldProperlyExecuteCommand() throws TelegramApiException {
+        //given
+        Long chatId = 1234567824356L;
+
+        Update update = new Update();
+        Message message = Mockito.mock(Message.class);
+        Mockito.when(message.getChatId()).thenReturn(chatId);
+        Mockito.when(message.getText()).thenReturn(getCommandName());
+        update.setMessage(message);
+
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId.toString());
+        sendMessage.setText(getCommandMessage());
+        sendMessage.enableHtml(true);
+
+        //when
+        getCommand().execute(update);
+
+        //then
+        Mockito.verify(bot).execute(sendMessage);
+    }
+}
---
+++ src/test/java/com/github/javarushcommunity/jrtb/service/SendBotMessageServiceTest.java
@@ -0,0 +1,41 @@
+package com.github.javarushcommunity.jrtb.service;
+
+
+import com.github.javarushcommunity.jrtb.bot.KostarTelegramBot;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@DisplayName("Unit-level testing for SendBotMessageService")
+public class SendBotMessageServiceTest {
+
+    private SendBotMessageService sendBotMessageService;
+    private KostarTelegramBot bot;
+
+    @BeforeEach
+    public void init() {
+        bot = Mockito.mock(KostarTelegramBot.class);
+        sendBotMessageService = new SendBotMessageServiceImpl(bot);
+    }
+
+    @Test
+    public void shouldProperlySendMessage() throws TelegramApiException {
+        //given
+        String chatId = "test_chat_id";
+        String message = "test_message";
+
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setText(message);
+        sendMessage.setChatId(chatId);
+        sendMessage.enableHtml(true);
+
+        //when
+        sendBotMessageService.sendMessage(chatId, message);
+
+        //then
+        Mockito.verify(bot).execute(sendMessage);
+    }
+}
